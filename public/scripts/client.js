fd6ce7856533bd4523bac00ddfadcfd30d830ea1
SophiaL1024 <sophiafengliu@gmail.com>
2021-05-19 00:28:02 +0000

    add creatTweetElement function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,43 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+
+$(document).ready(function() {
+
+  const createTweetElement = function(obj) {
+    const $tweet = $(`
+    <article class="tweet">
+        <header>
+          <span><img src=${obj.user.avatars}> ${obj.user.name}</span>
+          <span id="mention">${obj.user.handle}</span>
+        </header>
+        <p>${obj.content.text}</p>
+        <footer>
+          <span id="time-ago">${obj.created_at}</span>
+          <div><i class="fas fa-flag"></i><i class="fas fa-redo-alt"></i><i class="fas fa-heart"></i></div>
+        </footer>
+    </artical>
+    `);
+    return $tweet;
+  };
+
+    // Test / driver code (temporary). Eventually will get this from the server.
+  const tweetData = {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  }
+
+  const $tweet = createTweetElement(tweetData);
+
+  // Test / driver code (temporary)
+  console.log($tweet); // to see what it looks like
+  $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+})
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
-  $('#tweet-text').on('input',function() {
+  $('#tweet-text').on('input', function() {
     //calculate remaining characters by each input, and assign the value to counter
     $(this).parent().find('.counter').html(140 - $(this).val().length);
 
     //get the immediate value of counter, and check if it is negative
-    const counts = $(this).parent().find('.counter').html();    
+    const counts = $(this).parent().find('.counter').html();
     if (counts < 0) {
-      $(this).parent().find('.counter').addClass('negative');     
-    }else{
+      $(this).parent().find('.counter').addClass('negative');
+    } else {
       $(this).parent().find('.counter').removeClass('negative');
     }
   })
